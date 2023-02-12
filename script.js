a78fe3ac3554b84b05e64ed2d667c1b14e4d64d0
DawidGradowski <d.gradowski23@gmail.com>
2023-02-12 14:57:03 +0100

    Final commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function abc(a, b){
-    c = a + b;
-    return c;
-}      
-abc(3,4)
+const container = document.getElementsByClassName('container')[0]
+
+// Buttons
+const closeBtn = document.getElementById('close');
+const openBtn = document.getElementById('open');
+
+
+openBtn.addEventListener('click', () => {
+    container.classList.add("show-nav");
+})
+
+closeBtn.addEventListener('click', () => {
+    container.classList.remove("show-nav");
+})
